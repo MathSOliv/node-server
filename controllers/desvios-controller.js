32122afeb816239bf7ee96fe3d175b9a77a15e2b
MathSOliv <matheus.silvagab@gmail.com>
2022-11-01 16:14:22 -0300

    trying to turn the values into an array

--- controllers/desvios-controller.js
+++ controllers/desvios-controller.js
@@ -1,20 +1,22 @@
 const Desvios = require('../models/desvios')
 
 exports.desviosPorID = async (req, res, next) =>{
-    const desvio = await Desvios.findAll({
-        where: {
-            site: "034 - LD Celulose - Amadeus"
-        }
-    });
-    res.render("home.ejs", {desvio: desvio})
-}
 
+}
 
 exports.desviosPorObra = async (req, res, next) =>{
-    const desvio = await Desvios.findByPk(368);
-    res.render("home.ejs", {desvio: desvio})
+    const desvio = await Desvios.findAll({
+        where: {
+            site: "030 - Mosaic - Uberaba",
+            nivel: "Leve"
+        }
+    }).then((desvio) =>{
+        return res.render("home.ejs", {desvio: desvio})
+    }).catch((error) =>{
+        return console.log(error.message)
+    });
+   
 }
-
 
 exports.desvios = async (req, res, next) =>{
     const desvio = await Desvios.findByPk(368);
